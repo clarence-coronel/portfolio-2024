eb781ae041eb555a066024428cd208ac0f1de290
clarence-coronel <clarence.coronel.r@gmail.com>
2024-10-19 20:11:09 +0800

    refactor: clean up

--- src/components/navigation/components/icon-link.tsx
+++ src/components/navigation/components/icon-link.tsx
@@ -11,7 +11,7 @@ const IconLink = forwardRef<HTMLDivElement, IconLinkProps>(
     return (
       <div ref={ref} {...props} className="group relative">
         {children}
-        <span className="font-semibold pointer-events-none text-sm select-none group-hover:opacity-100 group-hover:-top-[110%] opacity-0 absolute bg-foreground text-background rounded-full px-2 py-1 left-1/2 -translate-x-[50%] top-[100%] duration-500">
+        <span className="truncate font-semibold pointer-events-none text-sm select-none group-hover:opacity-100 group-hover:-top-[110%] opacity-0 absolute bg-foreground text-background rounded-full px-2 py-1 left-1/2 -translate-x-[50%] top-[100%] duration-500">
           {title}
         </span>
       </div>
