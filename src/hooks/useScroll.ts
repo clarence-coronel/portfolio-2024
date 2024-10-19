0a0331a6cbae1480ca244649ac23b1cfae1da1b1
clarence-coronel <clarence.coronel.r@gmail.com>
2024-10-19 18:06:03 +0800

    feat: add custom hooks

---
+++ src/hooks/useScroll.ts
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+
+const useScroll = (threshold = 100) => {
+  const [hasScrolled, setHasScrolled] = useState(false);
+
+  useEffect(() => {
+    // Check if we're in the browser environment
+    if (typeof window !== "undefined") {
+      const handleScroll = () => {
+        // Check the scroll position
+        if (window.scrollY > threshold) {
+          setHasScrolled(true);
+        } else {
+          setHasScrolled(false); // Optionally reset on scroll back up
+        }
+      };
+
+      // Add the scroll event listener
+      window.addEventListener("scroll", handleScroll);
+
+      // Clean up the event listener on component unmount
+      return () => {
+        window.removeEventListener("scroll", handleScroll);
+      };
+    }
+  }, [threshold]);
+
+  return hasScrolled;
+};
+
+export default useScroll;
---
+++ src/hooks/useWindowSize.ts
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+
+const useWindowSize = () => {
+  // State to hold the window size
+  const [windowSize, setWindowSize] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
+
+  useEffect(() => {
+    // Handler to update the state with new window size
+    const handleResize = () => {
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    };
+
+    // Add event listener to update window size on resize
+    window.addEventListener("resize", handleResize);
+
+    // Cleanup: remove the event listener on unmount
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []); // Empty dependency array ensures this effect only runs once
+
+  return windowSize; // Return the current window size
+};
+
+export default useWindowSize;
